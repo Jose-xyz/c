3f8e4a8080c238a527a82befd89f511a7324ca8d
Jose Sosa <josenicolasosa@gmail.com>
2025-09-21 17:28:27 -0400

    finished compound interest calc

--- projects/compound_interest_calc.c
+++ projects/compound_interest_calc.c
@@ -4,11 +4,29 @@
 int main(){
     
     // compound interest calc
+    double principal = 0.0;
+    double rate = 0.0;
+    int years = 0;
+    int timeCompounded = 0;
+    double total = 0.0;
 
-
+    printf("Compound interest Calc\n");
     
-    printf("test");
+    printf("Enter principal (P): ");
+    scanf("%lf", &principal);
+    
+    printf("Enter interest rate percent (r): ");
+    scanf("%lf", &rate);
+    rate = rate / 100;
+
+    printf("Enter the # of years (t): ");
+    scanf("%d", &years);
+    printf("Enter # of time compounded per year (n): ");
+    scanf("%d", &timeCompounded);
+
+   total = principal * pow(1 + rate / timeCompounded, timeCompounded * years);
+
+   printf(" After %d years the total will be $%.2lf", years, total);
     
     return 0;
 }
-//test3
