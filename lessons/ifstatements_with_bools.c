23abff3878dde0fa6e598ee83506004fd8abce98
Jose Sosa <josenicolasosa@gmail.com>
2025-09-21 21:36:22 -0400

    added 09/21/2025

--- lessons/ifstatements_with_bools.c
+++ lessons/ifstatements_with_bools.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <string.h>
 
 int main(){
 
-printf("test");
+
+
+    // Example #1
+    bool isStudent = true;
+
+    if(isStudent){
+        printf("You are a student\n");
+    }
+    else{
+        printf("You are NOT a student\n");
+    }
+
+
+    // Example #2
+
+    char name[50] = "";
+
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) - 1] = '\0';
+
+    if(strlen(name) == 0){
+        printf("You did not enter your nombre!");
+    }
+    else{
+        printf("Hello %s", name);
+    }
+
+
+
+
 
     return 0;
 }
---
+++ projects/weight_convertor.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+// Weight converter app
+
+int main(){
+
+
+
+    return 0;
+}
