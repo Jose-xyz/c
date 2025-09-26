e9818f7d36b9f32dae416df442d1fadd8e358ef6
Jose Sosa <josenicolasosa@gmail.com>
2025-09-25 22:26:22 -0400

    added nested if statements

--- lessons/nested_if_statements.c
+++ lessons/nested_if_statements.c
@@ -1,10 +1,44 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 
 int main(){
 
+    float price = 10.00;
+    bool isStudent = false; // % discount
+    bool isSenior = true; // % discount
 
+    // Student = $9
+    // Senior = $8
+    // Student + Senior = $7
+    
+     if (isStudent)
+     {
+        if (isSenior)
+        {
+            printf("You get a student discount of 10%%\n");
+            printf("You get a senior discount of 20%%\n");
+            price *= 0.7;
+        }
+        else
+        {
+            printf("You get a student discount of 10%%");
+            price *= 0.9;
+        }
+     }
+     
+    else if (isSenior)
+    {
+        printf("You get a senior discount of 20%%\n");
+        price *= 0.8;
+    }
+        
+     else
+    {
+        printf("You are a working adult, pay full price!\n");
+    }
 
+     
+    printf("Price of ticket is is: $%.2f\n", price);
 
     return 0;
 }
