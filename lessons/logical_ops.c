e23e9c5b4b23ae38ca7d30595734988eec3dc69c
Jose Sosa <josenicolasosa@gmail.com>
2025-09-27 11:47:16 -0400

    added logical op file

---
+++ lessons/logical_ops.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main() {
+
+    // logical operators = used to combine or modify booleans expressions.
+
+    // && = AND
+    // || = OR
+    // !  = NOT
+
+
+
+    return 0;
+}
