43286452ea65b90fbe0a4076f483f7f9c00277bf
Jose Sosa <josenicolasosa@gmail.com>
2025-09-27 13:46:59 -0400

    finished log ops lesson

--- lessons/logical_ops.c
+++ lessons/logical_ops.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 
 int main() {
 
@@ -9,6 +9,43 @@ int main() {
     // || = OR
     // !  = NOT
 
+
+    int temp = 0;
+
+
+    printf("Enter todays temp: ");
+    scanf("%d", &temp);
+
+    // example of AND
+    // Both statements have to be true to execute code
+
+    if(temp > 0 && temp < 30){
+        printf("The temp is Goody\n");
+    }
+    else{
+        printf("The temp is Horrible!\n");
+    }
+
+    // example of OR
+    // One statement has to be true execute code
+
+    if(temp <= 0 || temp >= 30){
+        printf("The temp is BAD\n");
+    }
+    else{
+        printf("The temp is GOOD!\n");
+    }
+
+    // Using NOT with bools
+
+    bool isSuuny = true;
+
+    if(!isSuuny){
+        printf("It is CLOUDY inside");
+    }
+    else{
+        printf("It is SUNNY inside");
+    }
 
 
     return 0;
