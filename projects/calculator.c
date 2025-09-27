9a4a97482cc7a52d70af4a41982b71c10f00da84
Jose Sosa <josenicolasosa@gmail.com>
2025-09-27 11:37:53 -0400

    finished calc program

--- projects/calculator.c
+++ projects/calculator.c
@@ -3,6 +3,48 @@
 int main(){
     // calculator program
 
+    char operator = '\0'; // null terminator
+    double num1 = 0.0;
+    double num2 = 0.0;
+    double result = 0.0;
 
+    printf("Enter the first number: ");
+    scanf("%lf", &num1); 
+    
+    printf(" Enter the operator (+ - * /): ");
+    scanf(" %c", &operator); // clear \n from input buffer
+    
+    printf("Enter the : ");
+    scanf("%lf", &num2); 
+    
+    switch(operator){
+
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '/':                                // example of nested if statement in switch/case
+            if(num2 == 0){
+                printf("You can't divide by zero!\n");
+            }
+            else{
+                result = num1 / num2;
+            }
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        default:
+            printf("Invalid Operator\n");
+    }
+
+    printf("Result: %.4lf", result);
     return 0;
 }
+
+
+
+
+
