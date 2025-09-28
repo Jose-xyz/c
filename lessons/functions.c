9a7688b8c204263af47695a71e93dd0606901f05
Jose Sosa <josenicolasosa@gmail.com>
2025-09-27 22:04:59 -0400

    finished functions lessons

--- lessons/functions.c
+++ lessons/functions.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /*
 A function is a reusable section of code that can be invoked aka "Called"
@@ -10,26 +11,29 @@ Arguments can be sent to a function so that it can use them
 
 // Functions need parameter defined with the data type
 void happyBirthday(char name[], int age){
- printf("\nHappy Birthday to you!");
- printf("\nHappy Birthday to you!");
- printf("\nHappy Birthday dear %s!", name);
- printf("\nHappy Birthday to you!");
- printf("\nYou are %d years old", age);
+    printf("\nHappy Birthday to you!");
+    printf("\nHappy Birthday to you!");
+    printf("\nHappy Birthday dear %s!", name);
+    printf("\nHappy Birthday to you!");
+    printf("\nYou are %d years old", age);
 }
 
 
 
 int main(){
 
-    char name[] = "Jose";
-    int age = 25;
+    char name[50] = "";
+    int age = 0;
+
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) - 1] = '\0';
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
 
     // When calling the function, the parameters need to be the same data type
-
-
     happyBirthday(name, age);
-    // happyBirthday();
-    // happyBirthday();
 
 
 
