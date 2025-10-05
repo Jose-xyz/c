cb4ab794618ca4f61c2198ae9752231498941095
Jose Sosa <josenicolasosa@gmail.com>
2025-10-05 18:39:39 -0400

    finished randoms nums

--- lessons/random_nums.c
+++ lessons/random_nums.c
@@ -7,9 +7,20 @@ int main(){
 
     srand(time(NULL));
 
-    int min = 1;
-    int max = 6;
-    int randomNum = (rand() % max) + min;
+    int min = 50;
+    int max = 100;
+    int randomNum1 = (rand() % (max - min + 1)) + min;
+
+    int randomNum2 = (rand() % (max - min + 1)) + min;
+    int randomNum3 = (rand() % (max - min + 1)) + min;
+
+    printf("%d\n", randomNum1);
+    printf("%d\n", randomNum2);
+    printf("%d\n", randomNum3);
+
+    // print("%d %d %d",  randomNum1, randomNum2,  randomNum3); ----also valid
+
+    return 0;
 
 }
 
