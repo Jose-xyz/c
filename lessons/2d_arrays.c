79ef69510aaf95f0d4838ba4b2d85da7cca12c63
Jose Sosa <josenicolasosa@gmail.com>
2025-10-11 12:20:56 -0400

    started 2d arrays

--- lessons/2d_arrays.c
+++ lessons/2d_arrays.c
@@ -9,25 +9,30 @@ array[][] = {{}, {}, {}};
 
 
 int main(){
-
-    int numbers_1[] = {1, 2, 3}; // 1D array
-
-    int numbers_2[][3] = {{1, 2, 3},
-                          {4, 5, 6}, 
-                          {7, 8, 9}};     // 2D array, good if you need a matrix or grid of data, must add [3]for number of columns
 /*
+     int numbers_1[] = {1, 2, 3}; // 1D array
+
+     int numbers_2[][3] = {{1, 2, 3},
+                           {4, 5, 6}, 
+                           {7, 8, 9}};     // 2D array, good if you need a matrix or grid of data, must add [3]for number of columns
+
     printf("%d ", numbers_2[0][0]);
     printf("%d ", numbers_2[0][1]);
     printf("%d ", numbers_2[0][2]);
 */
 
+    char  numpad[][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'},{'*', '0','#'}};
+
+
+
+
+
     // nested for loop to print 2d array
     for (int i = 0; i < 3; i++) // rows  
     { 
-
         for (int j = 0; j < 3; j++) // columns  
         {
-        printf("%d ", numbers_2[i][j]);
+        printf("%d ", numpad[i][j]);
         }
         printf("\n");
     }
