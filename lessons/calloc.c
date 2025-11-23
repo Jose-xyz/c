19b3edb6283c9deb83096ecb078d1ce0f7ed7e69
Jose Sosa <josenicolasosa@gmail.com>
2025-11-23 00:18:55 -0500

    allocs

---
+++ lessons/calloc.c
@@ -0,0 +1,22 @@
+/*
+calloc() 
+Contiguous Allocation aka clear allocation
+
+Allocates memory dynamically and sets all allocated bytes to 0
+malloc() is faster but calloc() leads to less bugs
+
+calloc(#, size)
+*/
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+
+    return 0;
+}
